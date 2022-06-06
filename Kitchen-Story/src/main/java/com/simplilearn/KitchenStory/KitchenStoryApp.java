7225337605553a976ce57a17a96fa7969a2954eb
FatimaAlMufti <103098849+FatimaAlMufti@users.noreply.github.com>
2022-06-06 18:45:16 +0300

    backend
    backend of kitchen story

---
+++ Kitchen-Story/src/main/java/com/simplilearn/KitchenStory/KitchenStoryApp.java
@@ -0,0 +1,15 @@
+package com.simplilearn.KitchenStory;
+
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+	@SpringBootApplication
+	public class KitchenStoryApp {
+
+public static void main(String[] args) {
+			SpringApplication.run(KitchenStoryApp.class, args);
+			System.out.println("hellp");
+		}
+
+	}
+
